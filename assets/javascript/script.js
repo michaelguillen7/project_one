ad42827f2305baa3d7e3466b44a40632cebdde5c
Muscle Everest <danza_seppia@yahoo.com>
2018-08-10 14:18:37 -0500

    output of results coded

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,20 +1,33 @@
-var lat;
-var lng;
-
-if (navigator.geolocation) {
-  navigator.geolocation.getCurrentPosition(function (position) {
-    lat = position.coords.latitude;
-    lng = position.coords.longitude;
-
-  }, function () {
-    handleLocationError(true);
-  });
-} else {
-  // Browser doesn't support Geolocation
-  handleLocationError(false);
-};
-
 $(document).ready(function () {
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCvJGim2sDJ7_V7IHYZOPSSNis3OsPu2pM",
+    authDomain: "tender-1533827992506.firebaseapp.com",
+    databaseURL: "https://tender-1533827992506.firebaseio.com",
+    projectId: "tender-1533827992506",
+    storageBucket: "tender-1533827992506.appspot.com",
+    messagingSenderId: "647175454840"
+  };
+  firebase.initializeApp(config);
+
+  var db = firebase.database();
+  var lat;
+  var lng;
+
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function (position) {
+      lat = position.coords.latitude;
+      lng = position.coords.longitude;
+
+    }, function () {
+      handleLocationError(true);
+    });
+  } else {
+    // Browser doesn't support Geolocation
+    handleLocationError(false);
+  };
+
   var userDistance;
   var userPrice;
   var userCuisine;
@@ -28,7 +41,7 @@ $(document).ready(function () {
 
 
   // Google Places API Key: AIzaSyAb-0Pdg5FAuE2FKaehXYjFX3sjhvyyQto
-  $(document).on("click", "a", function () {
+  $("#submit-button").on("click", function () {
 
     userCuisine = $("#cuisine").val();
     userDistance = (parseInt($("#distance").val()) * 1609.344);
@@ -44,8 +57,11 @@ $(document).ready(function () {
         choiceList.push(data.results[item]);
       };
       returnChoice = choiceList[Math.floor(Math.random() * choiceList.length)];
-      console.log(returnChoice);
+      console.log(returnChoice);      
     });
+
+    // Store search results in Firebase
+    // db.ref().child("results").set(choiceList);
 
     //Insert query results onto Restaurant Details page
     $("#result-images").html(returnChoice.photos[0].html_attributions[0]);
